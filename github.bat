3092994a7939fdbbc71bb120ce040e31b04f72c2
SwastikKaushal <swastikkaushal2022008@gmail.com>
2025-04-12 16:04:45 +0530

    DISCORD BOT

--- github.bat
+++ github.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d %~dp0
 
-REM Set Git Username and Email (only needed once globally)
+REM Set Git Username and Email
 git config --global user.name "SwastikKaushal"
 git config --global user.email "swastikkaushal2022008@gmail.com"
 
@@ -12,8 +12,11 @@ echo Committing changes...
 set /p commit_msg="Enter commit message: "
 git commit -m "%commit_msg%"
 
+echo Pulling remote changes (rebase)...
+git pull --rebase origin main
+
 echo Pushing to GitHub...
-git push https://github.com/SwastikKaushal1/Custom-Discord-Bot.git main
+git push origin main
 
 echo All done! Press any key to exit...
 pause >nul
